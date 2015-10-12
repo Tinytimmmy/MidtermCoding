f4990362421b82969517a423d4e1bbab9929ee53
Tinytimmmy <tmarcus@udel.edu>
2015-10-12 03:34:36 -0400

    commit to github

--- MidtermStarterProject/src/main/java/com/cisc181/core/PersonException.java
+++ MidtermStarterProject/src/main/java/com/cisc181/core/PersonException.java
@@ -1,9 +1,26 @@
 package com.cisc181.core;
 
 public class PersonException extends Exception {
+	private String Person;
 	private Person testperson;
 
 	public PersonException(Person PersonExcep) {
-		this.testperson = PersonExcep;
+		this.setTestperson(PersonExcep);
+	}
+
+	public String getPerson() {
+		return Person;
+	}
+
+	public void setPerson(String person) {
+		Person = person;
+	}
+
+	public Person getTestperson() {
+		return testperson;
+	}
+
+	public void setTestperson(Person testperson) {
+		this.testperson = testperson;
 	}
 }
